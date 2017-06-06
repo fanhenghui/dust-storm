d42934f1eda23a6d6e107b840a28c6bcb9f961c1
wangrui22 <worry_wang@163.com>
2017-06-06 09:42:23 +0800

    add AnnoFileStats

---
+++ ASAP-Custom/Executables/DBImporter/db_importer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+
+namespace
+{
+
+}
+
+
+int main(int argc , char* argv[])
+{
+    //exe "ip" "username" "passport" "database" "file_root"
+
+}
