09a75dea1107fc0d94d68a17a22f8ebaa7f3d28d
wangrui22 <wangrui22@baidu.com>
2017-06-15 02:53:28 -0400

    makefile debug release

--- Linux_c/classes/class0.cpp
+++ Linux_c/classes/class0.cpp
@@ -1,10 +1,14 @@
 #include "class0.h"
 #include <boost/thread.hpp>
+//#define TEST
 
 namespace
 {
     void run()
     {
+#ifdef TEST
+        std::cout << "This is test.\n";
+#endif
         std::cout << "I am class 0 . in other thread.\n";
     }
 }
