e984a391704005fc117de70efbc29a1f38ab4a62
wangrui22 <worry_wang@163.com>
2017-06-25 23:14:49 +0800

    test web

--- Test/WebTest2/server.js
+++ Test/WebTest2/server.js
@@ -1,64 +1,73 @@
-// var http = require('http');
-// var fs = require('fs');
-// var url = require('url');
-
-// http.createServer( function (request , response){
-//     //解析域名，包括文件名
-//     var pathname = url.parse(request.url).pathname;
-
-//     //输出请求的文件名字 
-//     console.log('request for ' + pathname + ' received.');
-
-//     //从文件系统中读取请求的文件内容
-//     fs.readFile(pathname.substr(1) , function (err , data){
-//         if(err){
-//             console.log(err);
-//             //HTTP 状态码 : 404 NOT FOUND
-//             //Content Type : text/plain
-//             response.writeHead(404 , {'Content-Type': 'text/html'});
-//         }else {
-//             //HTTP 状态码 ： 200 OK
-//             //content type : text/plain
-//             response.writeHead(200 , {'Content-Type' : 'text/html'});
-//             response.write(data.toString());
-//         }
-//         //发送相应数据
-//         response.end();
-//     });
-// }).listen(8081);
-
-// console.log('Server running at http://127.0.0.1:8081/');
-
-
-var express = require('express');
+express = require("express");
+util = require("util");
+var bodyparser = require("body-parser");
 var app = express();
-var bodyParser = require('body-parser');
-var cp = require('child_process')
- 
-// 创建 application/x-www-form-urlencoded 编码解析
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
-app.use(express.static('public'));
- 
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
- 
-app.post('/process_post', urlencodedParser, function (req, res) {
- 
-   // 输出 JSON 格式
-   var response = {
+
+var urlencodedParser =bodyparser.urlencoded({extended:false});
+
+app.use(express.static("public"));
+
+app.get("/index.html" , function(req , res){
+    console.log("hostname : " + req.hostname);
+    console.log("ip : " + req.ip);
+    console.log("orignal URL : " + req.orignalUrl);
+    console.log("base URL : " + req.baseUrl);
+    console.log("path : " + req.path);
+    console.log("protocol : " + req.protocol);
+    console.log("query : " + util.inspect(req.query));
+    console.log("route : " + util.inspect(req.route));
+    console.log("subdomains : " + req.subdomains);
+    console.log("body : " + req.body);
+
+    //res.send("hello world");
+    res.sendFile(__dirname + "/" +"index.html");
+
+});
+
+app.get("/process_get" , function(req , res){
+
+    var response = { 
+        "firstname" : req.query.first_name ,
+        "lastname" : req.query.last_name 
+    };
+    console.log(util.inspect(response));
+    res.sendFile(__dirname + "/" + "jump.html");
+    //res.end(JSON.stringify(response));
+});
+
+app.post("/process_get" , urlencodedParser , function(req , res){
+
+    // var response = { 
+    //     "firstname2" : req.query.first_name ,
+    //     "lastname2" : req.query.last_name 
+    // };
+    // console.log(util.inspect(response));
+    // res.end(JSON.stringify(response));
+
+    var response = {
        "first_name":req.body.first_name,
        "last_name":req.body.last_name
    };
    console.log(response);
    res.end(JSON.stringify(response));
-})
- 
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
- 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
- 
-})
+});
+
+
+app.get("/ok" , function(req , res){
+
+    res.send("OK");
+});
+
+
+app.get("/error" , function(req , res){
+
+    res.send("error");
+});
+
+var server  = app.listen(8080 , function(){
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("address is http://%s:%s" , host , port);
+});
