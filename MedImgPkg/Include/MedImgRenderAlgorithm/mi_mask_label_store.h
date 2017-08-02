5f5a0a5ddee52cce72b558919d00d8d94dbf30ba
wangrui22 <wangrui22@DESKTOP-FDTN88>
2017-08-02 21:04:52 +0800

    fix linux build 2

--- MedImgPkg/Include/MedImgRenderAlgorithm/mi_mask_label_store.h
+++ MedImgPkg/Include/MedImgRenderAlgorithm/mi_mask_label_store.h
@@ -1,6 +1,7 @@
 #ifndef MED_IMG_LABEL_STORE_H_
 #define MED_IMG_LABEL_STORE_H_
 
+#include <vector>
 #include "MedImgRenderAlgorithm/mi_render_algo_export.h"
 #include "boost/thread/mutex.hpp"
 #include "boost/thread/locks.hpp"
