000343909f44979125833ef24892e1539041997b
worry_wang@163.com <worry_wang@163.com>
2017-08-14 03:33:43 -0400

    fix fe jpeg parse error

--- MedImgPkg/Executable/WebViewer/public/js/client.js
+++ MedImgPkg/Executable/WebViewer/public/js/client.js
@@ -4,6 +4,7 @@
     myCtx = myCanvas.getContext("2d");
     //myCanvasImg = myCtx.createImageData(myCanvas.width, myCanvas.height);
     myCanvasImgJpeg = new Image();
+    myDataJpeg = null;
 
     msgEnd = true;
     msgRest = 0;
@@ -18,7 +19,6 @@
     ipc_big_end = 0;
     ipc_data_len = 0;
 
-
     //FE to BE
     COMMAND_ID_FE_READY = 120001;
     COMMAND_ID_FE_OPERATION = 120002;
@@ -30,7 +30,29 @@
     COMMAND_ID_BE_READY = 270001;
     COMMAND_ID_BE_SEND_IMAGE = 270002;
 
+    function renderToCanvas(curImgLen, bufferOffset, arraybuffer) {
 
+        //Construct jpeg data
+        if (bufferOffset == 32) {
+            myDataJpeg = "";
+            //myDataJpeg = "data:image/jpg;base64,";
+        } else {
+            //console.log("second input");
+        }
+        //Draw jpeg buffer
+        var imgBuffer = new Uint8Array(arraybuffer, bufferOffset, curImgLen);
+        // var b64encoded = btoa(String.fromCharCode.apply(null, imgBuffer));
+        // myDataJpeg += b64encoded;
+        myDataJpeg += String.fromCharCode.apply(null, imgBuffer);
+
+        if (msgRest - curImgLen <= 0) {
+            myCanvasImgJpeg.src = "data:image/jpg;base64," + btoa(myDataJpeg);
+            myCanvasImgJpeg.onload = function() {
+                console.log("Image Onload");
+                myCtx.drawImage(myCanvasImgJpeg, 0, 0, myCanvas.width, myCanvas.height);
+            };
+        }
+    }
 
 
     window.FE = {
@@ -48,7 +70,7 @@
             this.username = username;
 
             //链接websocket服务器
-            this.socket = io.connect("http://172.23.237.253:8000");
+            this.socket = io.connect("http://172.23.237.109:8000");
 
             //通知服务器有用户登录
             this.socket.emit("login", { userid: this.userid, username: this.username });
@@ -105,22 +127,7 @@
                         //this.socket.emit
                     } else if (ipc_msg_id == COMMAND_ID_BE_SEND_IMAGE) {
                         //Draw jpeg buffer
-                        var imgBuffer = new Uint8Array(arraybuffer, bufferOffset, curImgLen);
-                        var b64encoded = btoa(String.fromCharCode.apply(null, imgBuffer));
-                        var datajpg = "data:image/jpg;base64," + b64encoded;
-
-                        //var img = new Image();
-                        myCanvasImgJpeg.src = datajpg;
-                        myCanvasImgJpeg.onload = function() {
-                            console.log("Image Onload");
-                            myCtx.drawImage(myCanvasImgJpeg, 0, 0, myCanvas.width, myCanvas.height);
-                        };
-
-                        ////Draw raw image 
-                        //var imgBuffer = new Uint8Array(arraybuffer , bufferOffset, curImgLen);
-                        // for(var i = 0 ; i< curImgLen ; ++i){
-                        // myCanvasImg.data[msgLen - msgRest + i] = imgBuffer[i];
-                        // }
+                        renderToCanvas(curImgLen, bufferOffset, arraybuffer);
                     }
 
                 }
@@ -130,7 +137,6 @@
                     msgRest = 0;
                     msgLen = 0;
                     msgEnd = true;
-                    myCtx.putImageData(myCanvasImg, 0, 0);
                 }
             });
 
