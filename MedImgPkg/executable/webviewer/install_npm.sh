c34ef2673c4f901a825327741d05fba50a8ff77b
zhangchanggong <zhangchanggong@baidu.com>
2017-09-01 10:14:59 +0800

    add npm install script

--- MedImgPkg/executable/webviewer/install_npm.sh
+++ MedImgPkg/executable/webviewer/install_npm.sh
@@ -1,10 +1,19 @@
 npm install express --save
-npm install serve - favicon --save
+npm install path --save
+npm install http --save
+npm install socket.io --save
+npm install util --save
+npm install fs --save
 npm install morgan --save
-npm install cookie - parser --save
-npm install body - parser --save
-npm install --save multer@0
 npm install mongoose@4.1 --save
-npm install express - session --save
+npm install serve-favicon --save
+npm install express-session --save
+npm install child_process --save
+npm install cookie-parser --save
+npm install body-parser --save
+npm install node-ipc --save
+npm install multer@0 --save
 npm install ejs --save
+npm install ipaddr.js --save
+
 sudo apt - get install mongodb mongodb - clients mongodb - server
