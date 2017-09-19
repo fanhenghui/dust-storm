a528bde35c13fcc59d48a696767942ff1b8c99ae
wangrui22 <wangrui22@baidu.com>
2017-09-19 18:58:06 +0800

    format

--- MedImgPkg/executable/webviewer/server.js
+++ MedImgPkg/executable/webviewer/server.js
@@ -37,19 +37,6 @@ app.use(require('express').static(__dirname + '/public'));
 app.set('views', path.join(__dirname, '/public/views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
-
-// //主页的响应
-// app.get('/index.html', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-//   console.log('get index.html');
-// });
-
-// app.get('/review.html', function(req, res) {
-//   res.sendFile(__dirname + '/review.html');
-//   console.log('get review.html');
-// });
-
 app.use('/', routes);
 //登录表单的提交
 // var uRLEncoderParser = bodyParser.urlencoded({extended: false});
@@ -79,124 +66,106 @@ var COMMAND_ID_FE_SHUT_DOWN = 120000;
 var COMMAND_ID_FE_READY = 120001;
 ////////////////////////////////////////////////////////////
 
-io.on(
-    'connection',
-    function(socket) {
-      console.log('<><><><><><> connecting <><><><><><>');
+io.on( 'connection', function(socket) {
+  console.log('<><><><><><> connecting <><><><><><>');
+  //监听用户加入
+  socket.on('login', function(obj) {
+    if (obj.username == '') {
+      console.log('invalid null username!');
+      return;
+    }
 
-      var thisMsgEnd = 0;  // 0 msg header 1 data for last msg 2 msg header for
-                           // last msg header
-      var msgLen = 0;
-      var lastMsgRest = 0;
+    socket.name = obj.userid;  // uid to locate web socket
+    console.log('<><><><><><> logining <><><><><><>');
+    console.log(obj.username + ' has login.');
+    console.log('id : ' + obj.userid);
 
-      var lastMsgDataType = 0;
-      var lastMsgBigEnd = 0;
-      var lastMsgDataLen = 0;
+    if (!onlineUsers.hasOwnProperty(obj.userid)) {
+      onlineUsers[obj.userid] = obj.username;
+      onlineCount++;
+    } else {
+      console.log(obj.username + ' is online now.');
+    }
 
-      var lastMsgCmdID = 0;
-      var lastHeader = new Buffer(32);
-      var lastHeaderByteLength = 0;
+    // create a server IPC between server and BE
+    var nodeIpc = require('node-ipc');
+    const ipc = new nodeIpc.IPC;
 
-      //监听用户加入
-      socket.on('login', function(obj) {
-        if (obj.username == '') {
-          console.log('invalid null username!');
-          return;
+    console.log('UNIX path is : ' + obj.username);
+    ipc.config.id = obj.username;
+    ipc.config.retry = 1500;
+    ipc.config.rawBuffer = true;
+    ipc.config.encoding = 'hex';
+    ipc.serve(function() {
+      ipc.server.on('connect', function(local_socket) {
+        // when IPC is constructed between server and BE
+        // server send a ready MSG to BE to make sure BE is ready
+        console.log('IPC connect. Send FE ready to BE.');
+
+        const msgFEReady = new Buffer(32);
+        msgFEReady.writeUIntLE(1, 0, 4);
+        msgFEReady.writeUIntLE(0, 4, 4);
+        msgFEReady.writeUIntLE(COMMAND_ID_FE_READY, 8, 4);
+        msgFEReady.writeUIntLE(0, 12, 4);
+        msgFEReady.writeUIntLE(0, 16, 4);
+        msgFEReady.writeUIntLE(0, 20, 4);
+        msgFEReady.writeUIntLE(0, 24, 4);
+        msgFEReady.writeUIntLE(0, 28, 4);
+
+        if (ipc != undefined) {
+          // close IPC
+          ipc.server.emit(local_socket, msgFEReady);
+          //ipc.server.stop(); // @r.wang why close it here? commented by c.zhang
         }
-
-        socket.name = obj.userid;  // uid to locate web socket
-
-        console.log('<><><><><><> logining <><><><><><>');
-        console.log(obj.username + ' has login.');
-        console.log('id : ' + obj.userid);
-
-        if (!onlineUsers.hasOwnProperty(obj.userid)) {
-          onlineUsers[obj.userid] = obj.username;
-          onlineCount++;
-        } else {
-          console.log(obj.username + ' is online now.');
-          }
-
-        // create a server IPC between server and BE
-        var nodeIpc = require('node-ipc');
-        const ipc = new nodeIpc.IPC;
-
-        console.log('UNIX path is : ' + obj.username);
-        ipc.config.id = obj.username;
-        ipc.config.retry = 1500;
-        ipc.config.rawBuffer = true;
-        ipc.config.encoding = 'hex';
-
-        ipc.serve(function() {
-          ipc.server.on('connect', function(local_socket) {
-            // when IPC is constructed between server and BE
-            // server send a ready MSG to BE to make sure BE is ready
-            console.log('IPC connect. Send FE ready to BE.');
-
-            const msgFEReady = new Buffer(32);
-            msgFEReady.writeUIntLE(1, 0, 4);
-            msgFEReady.writeUIntLE(0, 4, 4);
-            msgFEReady.writeUIntLE(COMMAND_ID_FE_READY, 8, 4);
-            msgFEReady.writeUIntLE(0, 12, 4);
-            msgFEReady.writeUIntLE(0, 16, 4);
-            msgFEReady.writeUIntLE(0, 20, 4);
-            msgFEReady.writeUIntLE(0, 24, 4);
-            msgFEReady.writeUIntLE(0, 28, 4);
-
-            if (ipc != undefined) {
-              // close IPC
-              ipc.server.emit(local_socket, msgFEReady);
-              //ipc.server.stop(); // @r.wang why close it here? commented by c.zhang
-            }
-            onlineLocalSockets[obj.userid] = local_socket;
-          });
-
-          // sever get logic process's tcp package and transfer to FE
-          // directly(not parse)
-          ipc.server.on('data', function(buffer, local_socket) {
-            ipc.log('got a data : ' + buffer.length);
-            socket.emit('data', buffer);
-          });
-
-          ipc.server.on(
-            'socket.disconnected', function(local_socket, destroyedSocketID) {
-              ipc.log('client ' + destroyedSocketID + ' has disconnected!');
-            });
-        });
-        ipc.server.start();
-        onlineLocalIPC[obj.userid] = ipc;
-
-        // Create BE process
-        console.log( 'path: ' + '/tmp/app.' + obj.username);
-
-        // reader BE process path form config file
-        var review_server_path;
-        fs.readFile(__dirname + '/public/config/run_time.config', function(err, data) {
-          if (err) {
-            throw err;
-          }
-          review_server_path = data;
-          console.log('app path : ' + review_server_path);
-
-          // TODO Test 自己起C++ BE
-          //  const out_log = fs.openSync(
-          //      './out_' + util.inspect(obj.username) + '.log', 'a');
-          //  const err_log = fs.openSync(
-          //      './err_' + util.inspect(obj.username) + '.log', 'a');
-          //  var worker = childProcess.spawn(
-          //      review_server_path.toString(), ['/tmp/app.' +
-          //      obj.username],
-          //      {detached: true, stdio: ['ignore', out_log, err_log]});
-          //  onlineLogicProcessID[obj.userid] = worker;
-          //  console.log('<><><><><><> login in success <><><><><><>');
-        // var worker = childProcess.spawn(
-        //     review_server_path.toString(), ['/tmp/app.' +
-        //     obj.username],
-        //     {detached: true});
-        // onlineLogicProcessID[obj.userid] = worker;
-        console.log('<><><><><><> login in success <><><><><><>');
-          });
+        onlineLocalSockets[obj.userid] = local_socket;
       });
+
+      // sever get logic process's tcp package and transfer to FE
+      // directly(not parse)
+      ipc.server.on('data', function(buffer, local_socket) {
+        ipc.log('got a data : ' + buffer.length);
+        socket.emit('data', buffer);
+      });
+
+      ipc.server.on('socket.disconnected', function(local_socket, destroyedSocketID) {
+        ipc.log('client ' + destroyedSocketID + ' has disconnected!');
+      });
+    });
+    
+    ipc.server.start();
+    onlineLocalIPC[obj.userid] = ipc;
+
+    // Create BE process
+    console.log( 'path: ' + '/tmp/app.' + obj.username);
+    // reader BE process path form config file
+    var review_server_path;
+    fs.readFile(__dirname + '/public/config/run_time.config', function(err, data) {
+      if (err) {
+        throw err;
+      }
+      review_server_path = data;
+      console.log('app path : ' + review_server_path);
+
+      //// TODO Test 自己起C++ BE
+      // const out_log = fs.openSync(
+      //     './out_' + util.inspect(obj.username) + '.log', 'a');
+      // const err_log = fs.openSync(
+      //     './err_' + util.inspect(obj.username) + '.log', 'a');
+      // var worker = childProcess.spawn(
+      //     review_server_path.toString(), ['/tmp/app.' +
+      //     obj.username],
+      //     {detached: true, stdio: ['ignore', out_log, err_log]});
+      // onlineLogicProcessID[obj.userid] = worker;
+      // console.log('<><><><><><> login in success <><><><><><>');
+      // var worker = childProcess.spawn(
+      //     review_server_path.toString(), ['/tmp/app.' +
+      //     obj.username],
+      //     {detached: true});
+      // onlineLogicProcessID[obj.userid] = worker;
+
+      console.log('<><><><><><> login in success <><><><><><>');
+    });
+  });
 
       //监听用户退出
       socket.on('disconnect', function(obj) {
