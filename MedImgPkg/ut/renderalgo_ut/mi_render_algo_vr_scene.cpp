36ee00cf1b0998d203816c2f327817cd188fa174
wangrui22 <wangrui22@baidu.com>
2017-10-25 19:47:44 +0800

    add test mem leak

--- MedImgPkg/ut/renderalgo_ut/mi_render_algo_vr_scene.cpp
+++ MedImgPkg/ut/renderalgo_ut/mi_render_algo_vr_scene.cpp
@@ -271,9 +271,9 @@ void Display() {
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //std::shared_ptr<CameraBase> camera = _scene->get_camera();
-        //Quat4 q(5.0 / 360.0 * 2.0 * 3.1415926, Vector3(0, 1, 0));
-        //camera->rotate(q);
+        std::shared_ptr<CameraBase> camera = _scene->get_camera();
+        Quat4 q(5.0 / 360.0 * 2.0 * 3.1415926, Vector3(0, 1, 0));
+        camera->rotate(q);
 
         CHECK_GL_ERROR;
 
@@ -294,7 +294,7 @@ void Display() {
         //_time_query2->begin();
         // glBindFramebuffer(GL_DRAW_FRAMEBUFFER , 0);
 
-        _scene->download_image_buffer();
+        //_scene->download_image_buffer();
 
         CHECK_GL_ERROR;
         _scene->swap_image_buffer();
@@ -310,7 +310,7 @@ void Display() {
 #ifdef WIN32
         FileUtil::write_raw("D:/temp/output_ut.jpeg", buffer, buffer_size);
 #else
-        FileUtil::write_raw("/home/wangrui22/data/output_ut.jpeg", buffer, buffer_size);
+        //FileUtil::write_raw("/home/wangrui22/data/output_ut.jpeg", buffer, buffer_size);
 #endif
         // MI_RENDERALGO_LOG(MI_DEBUG) << "compressing time : " << _scene->get_compressing_duration() <<
         // ", buffer size: " << buffer_size;
@@ -437,7 +437,7 @@ void resize(int x, int y) {
 }
 
 void Idle() {
-    //glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void MouseClick(int button, int status, int x, int y) {
