63e770edc4cf71b70ff45691fe0fdc4bc054e355
wangrui22 <worry_wang@163.com>
2017-10-28 09:25:14 +0800

    convert cell ruler to ES6

--- MedImgPkg/executable/webviewer/public/js/mi-vertical-ruler.js
+++ MedImgPkg/executable/webviewer/public/js/mi-vertical-ruler.js
@@ -1,4 +1,4 @@
-const MarginRight = 15;
+const MARGIN_RIGHT = 15;
 
 function VerticalRuler(svg, cellID) {
     this.svg = svg;
@@ -25,14 +25,14 @@ Math.fmod = function (a, b) {
 };
 
 function decimalFloor(number) {
-    var power = 1.0;
+    let power = 1.0;
     while (number >= power) {
         power *= 10.0;
     }
     power /= 10.0;
-    var mod = Math.fmod(number, power);
-    var r = number - mod;
-    var divider = parseInt(Math.floor(r / power * 10.0 + 0.5));
+    let mod = Math.fmod(number, power);
+    let r = number - mod;
+    let divider = parseInt(Math.floor(r / power * 10.0 + 0.5));
 
     return {
         'length': r,
@@ -42,28 +42,28 @@ function decimalFloor(number) {
 }
 
 VerticalRuler.prototype.updateRuler = function (frustumHeight) {
-    var width = this.svg.getAttribute('width');
-    var height = this.svg.getAttribute('height');
+    const width = this.svg.getAttribute('width');
+    const height = this.svg.getAttribute('height');
 
-    var cx = width / 2.0;
-    var cy = height / 2.0;
-    var y1 = height / 4.0;
-    var y2 = 3.0 * height / 4.0;
+    let cx = width / 2.0;
+    let cy = height / 2.0;
+    let y1 = height / 4.0;
+    let y2 = 3.0 * height / 4.0;
 
-    var initialRulerLength = frustumHeight / 2.0; // in world space
-    var rulerSetting = decimalFloor(initialRulerLength);
-    var add = (y2 - y1) / initialRulerLength * rulerSetting.length;
-    var y1new = cy - add / 2;
-    var y2new = cy + add / 2;
+    let initialRulerLength = frustumHeight / 2.0; // in world space
+    let rulerSetting = decimalFloor(initialRulerLength);
+    let add = (y2 - y1) / initialRulerLength * rulerSetting.length;
+    let y1new = cy - add / 2;
+    let y2new = cy + add / 2;
 
     if ((y2 - y1) > 128) // at least 128 pixels 
     {
-        var xunit = width / 100.0; // 1 percentage of the window width in pixel
+        let xunit = width / 100.0; // 1 percentage of the window width in pixel
         if (xunit > 8.0) {
             xunit = 8.0;
         }
-        var smallAdd = 0;
-        var bigAdd = 0;
+        let smallAdd = 0;
+        let bigAdd = 0;
 
         if (((y2new - y1new) * 10.0 / rulerSetting.divider) > 4) {
             smallAdd = 0;
@@ -74,25 +74,25 @@ VerticalRuler.prototype.updateRuler = function (frustumHeight) {
             bigAdd = parseInt(Math.floor(xunit * 2.0));
         }
 
-        var pathCommands = 'M ' + (width - MarginRight - xunit - bigAdd) + ' ' + y1new + ' ';
-        pathCommands += 'L ' + (width - MarginRight) + ' ' + y1new + ' ';
-        pathCommands += 'L ' + (width - MarginRight) + ' ' + y2new + ' ';
-        pathCommands += 'L ' + (width - MarginRight - xunit - bigAdd) + ' ' + y2new + ' ';
+        let pathCommands = 'M ' + (width - MARGIN_RIGHT - xunit - bigAdd) + ' ' + y1new + ' ';
+        pathCommands += 'L ' + (width - MARGIN_RIGHT) + ' ' + y1new + ' ';
+        pathCommands += 'L ' + (width - MARGIN_RIGHT) + ' ' + y2new + ' ';
+        pathCommands += 'L ' + (width - MARGIN_RIGHT - xunit - bigAdd) + ' ' + y2new + ' ';
 
-        var yadd = add / rulerSetting.divider;
+        let yadd = add / rulerSetting.divider;
 
-        var ypos = y1new;
-        for (var i = 0; i < rulerSetting.divider; i += 10) {
-            pathCommands += 'M ' + (width - bigAdd - MarginRight) + ' ' + ypos + ' ';
-            pathCommands += 'L ' + (width - MarginRight) + ' ' + ypos + ' ';
+        let ypos = y1new;
+        for (let i = 0; i < rulerSetting.divider; i += 10) {
+            pathCommands += 'M ' + (width - bigAdd - MARGIN_RIGHT) + ' ' + ypos + ' ';
+            pathCommands += 'L ' + (width - MARGIN_RIGHT) + ' ' + ypos + ' ';
             ypos += yadd * 10;
         }
 
         ypos = y1new;
-        for (var i = 0; i < rulerSetting.divider; i++) {
+        for (let i = 0; i < rulerSetting.divider; i++) {
             if (i % 10 != 0) {
-                pathCommands += 'M ' + (width - smallAdd - MarginRight) + ' ' + ypos + ' ';
-                pathCommands += 'L ' + (width - MarginRight) + ' ' + ypos + ' ';
+                pathCommands += 'M ' + (width - smallAdd - MARGIN_RIGHT) + ' ' + ypos + ' ';
+                pathCommands += 'L ' + (width - MARGIN_RIGHT) + ' ' + ypos + ' ';
             }
             ypos += yadd;
         }
@@ -104,7 +104,7 @@ VerticalRuler.prototype.updateRuler = function (frustumHeight) {
 
         this.rulerLabel.text(rulerSetting.label)
             .attr('x', function (d) {
-                return width - MarginRight;
+                return width - MARGIN_RIGHT;
             })
             .attr('y', y2new+4);
     } else {
