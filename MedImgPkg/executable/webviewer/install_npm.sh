1cb526ceecf52e5283c0121bec29cc85e1b843eb
wangrui22 <wangrui22@baidu.com>
2017-11-21 13:13:30 +0800

    change npm pkg

--- MedImgPkg/executable/webviewer/install_npm.sh
+++ MedImgPkg/executable/webviewer/install_npm.sh
@@ -1,6 +1,5 @@
 npm install express --save
 npm install path --save
-npm install http --save
 npm install socket.io --save
 npm install util --save
 npm install fs --save
@@ -12,9 +11,7 @@ npm install cookie-parser --save
 npm install body-parser --save
 npm install node-ipc --save
 npm install ejs --save
-npm install ipaddr.js --save
-npm install log4js --save
 npm install mysql --save
-sudo npm install iconv-lite -g
+#sudo npm install iconv-lite -g
 sudo npm install pm2 -g  
 sudo npm install bower -g
