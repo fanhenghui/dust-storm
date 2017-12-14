d6ac4a715e58b6682d7d2700706280fcd60b0605
wangrui22 <wangrui22@baidu.com>
2017-12-14 16:06:01 +0800

    change socket client js member paramter to private

--- MedImgPkg/executable/webviewer/public/js/mi-socket-client.js
+++ MedImgPkg/executable/webviewer/public/js/mi-socket-client.js
@@ -2,94 +2,95 @@ const MSG_HEADER_LEN = 32;
 
 class SocketClient {
     constructor(socket) {
-        this.tcpPacketEnd = 0;
-        this.msgCmdID = 0;
-        this.msgCellID = 0;
-        this.msgOpID = 0;
-        this.msgRestDataLen = 0;
-        this.lastMsgHeader = new ArrayBuffer(MSG_HEADER_LEN);
-        this.lastMsgHeaderLen = 0;
+        this._tcpPacketEnd = 0;
+        this._msgCmdID = 0;
+        this._msgCellID = 0;
+        this._msgOpID = 0;
+        this._msgRestDataLen = 0;
+        this._lastMsgHeader = new ArrayBuffer(MSG_HEADER_LEN);
+        this._lastMsgHeaderLen = 0;
+        this._socket = socket;
+        
         this.protocRoot = null;
-        this.socket = socket;
     }
 
     //Handler(cmdID, cellID, opID, tcpBuffer, bufferOffset, dataLen, restDataLen, withHeader)
     recvData(tcpBuffer, msgHandler) {
         const tcpPackageLen = tcpBuffer.byteLength;
     
-        if (this.tcpPacketEnd == 0) {
+        if (this._tcpPacketEnd == 0) {
             if (tcpPackageLen < MSG_HEADER_LEN) { //incompleted Msg header
-                let dstBuffer = new Uint8Array(this.lastMsgHeader);
+                let dstBuffer = new Uint8Array(this._lastMsgHeader);
                 let srcBuffer = new Uint8Array(tcpBuffer)
                 for (let i = 0; i< tcpPackageLen; ++i) {
                     dstBuffer[i] = srcBuffer[i];
                 }
-                this.tcpPacketEnd = 2;
-                this.lastMsgHeaderLen = tcpPackageLen;
+                this._tcpPacketEnd = 2;
+                this._lastMsgHeaderLen = tcpPackageLen;
                 return;
             }
             let header = new Uint32Array(tcpBuffer, 0, 8);
-            this.msgCmdID = header[2];
-            this.msgCellID = header[3];
-            this.msgOpID = header[4];
+            this._msgCmdID = header[2];
+            this._msgCellID = header[3];
+            this._msgOpID = header[4];
             const lastMsgDatalen = header[7];
     
             if (tcpPackageLen - MSG_HEADER_LEN == lastMsgDatalen) { // completed one Msg
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, MSG_HEADER_LEN, lastMsgDatalen, 0, true);
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, MSG_HEADER_LEN, lastMsgDatalen, 0, true);
             } else if (tcpPackageLen - MSG_HEADER_LEN < lastMsgDatalen) { // not completed one Msg
-                this.msgRestDataLen = lastMsgDatalen - (tcpPackageLen - MSG_HEADER_LEN);
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, MSG_HEADER_LEN, tcpPackageLen - MSG_HEADER_LEN, this.msgRestDataLen, true);
-                this.tcpPacketEnd = 1;
+                this._msgRestDataLen = lastMsgDatalen - (tcpPackageLen - MSG_HEADER_LEN);
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, MSG_HEADER_LEN, tcpPackageLen - MSG_HEADER_LEN, this._msgRestDataLen, true);
+                this._tcpPacketEnd = 1;
             } else { // this buffer carry next Msg process current one
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, MSG_HEADER_LEN, lastMsgDatalen, 0, true);
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, MSG_HEADER_LEN, lastMsgDatalen, 0, true);
                 // recursion process rest
                 let tcpBufferSub = tcpBuffer.slice(lastMsgDatalen + MSG_HEADER_LEN);
-                this.tcpPacketEnd = 0;
+                this._tcpPacketEnd = 0;
                 this.recvData(tcpBufferSub, msgHandler);
             }
-        } else if (this.tcpPacketEnd == 1) { // data for last msg
-            if (tcpPackageLen - this.msgRestDataLen == 0) { // complete last msg
-                this.msgRestDataLen = 0;
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, 0, tcpPackageLen, 0, false);
-            } else if (tcpPackageLen - this.msgRestDataLen < 0) { // not complete data yet
-                this.msgRestDataLen -= tcpPackageLen;
-                this.tcpPacketEnd = 1;
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, 0, tcpPackageLen, this.msgRestDataLen, false);
+        } else if (this._tcpPacketEnd == 1) { // data for last msg
+            if (tcpPackageLen - this._msgRestDataLen == 0) { // complete last msg
+                this._msgRestDataLen = 0;
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, 0, tcpPackageLen, 0, false);
+            } else if (tcpPackageLen - this._msgRestDataLen < 0) { // not complete data yet
+                this._msgRestDataLen -= tcpPackageLen;
+                this._tcpPacketEnd = 1;
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, 0, tcpPackageLen, this._msgRestDataLen, false);
             } else { // this buffer carry next Msg
-                msgHandler(this.msgCmdID, this.msgCellID, this.msgOpID, tcpBuffer, 0, this.msgRestDataLen, 0, false);
-                let tcpBufferSub2 = tcpBuffer.slice(this.msgRestDataLen);
-                this.msgRestDataLen = 0;
-                this.tcpPacketEnd = 0;
+                msgHandler(this._msgCmdID, this._msgCellID, this._msgOpID, tcpBuffer, 0, this._msgRestDataLen, 0, false);
+                let tcpBufferSub2 = tcpBuffer.slice(this._msgRestDataLen);
+                this._msgRestDataLen = 0;
+                this._tcpPacketEnd = 0;
                 this.recvData(tcpBufferSub2, msgHandler);
             }
-        } else if (this.tcpPacketEnd == 2) { // msg header for last msg header
-            const lastRestHeaderLen = MSG_HEADER_LEN - this.lastMsgHeaderLen;
+        } else if (this._tcpPacketEnd == 2) { // msg header for last msg header
+            const lastRestHeaderLen = MSG_HEADER_LEN - this._lastMsgHeaderLen;
             if (tcpPackageLen < lastRestHeaderLen) { // msg header is not completed yet
-                let dstBuffer = new Uint8Array(this.lastMsgHeader);
+                let dstBuffer = new Uint8Array(this._lastMsgHeader);
                 let srcBuffer = new Uint8Array(tcpBuffer)
                 for (let i = 0 ; i< tcpPackageLen; ++i) {
-                    dstBuffer[i+this.lastMsgHeaderLen] = srcBuffer[i];
+                    dstBuffer[i+this._lastMsgHeaderLen] = srcBuffer[i];
                 }
-                this.tcpPacketEnd = 2;
-                this.lastMsgHeaderLen += tcpPackageLen;
+                this._tcpPacketEnd = 2;
+                this._lastMsgHeaderLen += tcpPackageLen;
                 return;
             } else { // msg header is completed
                 //fill header completed
-                let dstBuffer = new Uint8Array(this.lastMsgHeader);
+                let dstBuffer = new Uint8Array(this._lastMsgHeader);
                 let srcBuffer = new Uint8Array(tcpBuffer,0,lastRestHeaderLen);
                 for (let i = 0; i< lastRestHeaderLen; ++i) {
-                    dstBuffer[i+this.lastMsgHeaderLen] = srcBuffer[i];
+                    dstBuffer[i+this._lastMsgHeaderLen] = srcBuffer[i];
                 }
     
                 let tcpBufferSub3 = tcpBuffer.slice(lastRestHeaderLen);
-                let header2 = new Uint32Array(this.lastMsgHeader, 0, 8);
-                this.msgCmdID = header2[2];
-                this.msgCellID = header2[3];
-                this.msgOpID = header2[4];
-                this.msgRestDataLen = header2[7];
+                let header2 = new Uint32Array(this._lastMsgHeader, 0, 8);
+                this._msgCmdID = header2[2];
+                this._msgCellID = header2[3];
+                this._msgOpID = header2[4];
+                this._msgRestDataLen = header2[7];
     
-                this.tcpPacketEnd = 1;
-                this.lastMsgHeaderLen = 0;
+                this._tcpPacketEnd = 1;
+                this._lastMsgHeaderLen = 0;
                 this.recvData(tcpBufferSub3, msgHandler);
             }
         }
@@ -109,9 +110,9 @@ class SocketClient {
             header[7] = 0;
     
             //console.log('emit data length 0.');
-            this.socket.emit('data', {
-                userid: this.socket.userID,
-                username: this.socket.userName,
+            this._socket.emit('data', {
+                userid: this._socket.userID,
+                username: this._socket.userName,
                 content: headerBuffer
             });
         } else {
@@ -133,9 +134,9 @@ class SocketClient {
                 dstBuffer[index] = srcBuffer[index];
             }
             //console.log('emit data length' + dataLen + '.');
-            this.socket.emit('data', {
-                userid: this.socket.userID,
-                username: this.socket.userName,
+            this._socket.emit('data', {
+                userid: this._socket.userID,
+                username: this._socket.userName,
                 content: cmdBuffer
             });
         }
@@ -143,9 +144,9 @@ class SocketClient {
 
     //heartbeat 
     heartbeat() {
-        this.socket.emit('heartbeat', {
-            userid: this.socket.userID,
-            username: this.socket.userName
+        this._socket.emit('heartbeat', {
+            userid: this._socket.userID,
+            username: this._socket.userName
         });
     }
 
